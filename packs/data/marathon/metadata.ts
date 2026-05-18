b881b018426df21638d53f523d1af47d8a9335e9
theaddon <theaddonn@gmail.com>
2026-05-18 09:25:31 +0200

    fix: license shipping

--- packs/data/marathon/metadata.ts
+++ packs/data/marathon/metadata.ts
@@ -1,6 +1,11 @@
 import { join } from 'jsr:@std/path@^1';
 
 const src = Deno.env.get('ROOT_DIR')!;
+const bp = Deno.env.get('MARATHON_BP_DIR')!;
+const rp = Deno.env.get('MARATHON_RP_DIR')!;
 
-Deno.copyFile(join(src, 'LICENSE.md'), join('BP', 'LICENSE.md'));
-Deno.copyFile(join(src, 'TRADEMARK.md'), join('BP', 'TRADEMARK.md'));
+Deno.copyFile(join(src, 'LICENSE.md'), join(bp, 'LICENSE.md'));
+Deno.copyFile(join(src, 'TRADEMARK.md'), join(bp, 'TRADEMARK.md'));
+
+Deno.copyFile(join(src, 'LICENSE.md'), join(rp, 'LICENSE.md'));
+Deno.copyFile(join(src, 'TRADEMARK.md'), join(rp, 'TRADEMARK.md'));
